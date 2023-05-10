325c25149eef6f55537fc2706fde9dfdb250b140
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-10 11:32:54 +0700

    siwa memiliki class dan memiliki privat id metod setId

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -6,3 +6,16 @@ class siswa;
 class orang {
 private:
 	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
+
+class siswa {
+private:
+	int Id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
