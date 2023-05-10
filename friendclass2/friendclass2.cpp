4dc77d4552e42a75221f88cf019560e10145f64e
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-10 11:50:09 +0700

    membuat friendclass dengan astrick

---
+++ friendclass2/friendclass2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	string nama;
+public:
+	friend void setNama(mahasiswa& a, string);
+};
+
+void setNama(mahasiswa& a, string pNama) {
+	a.nama = pNama;
+	cout << "Nama: " << a.nama;
+}
+
+int main()
+{
+	mahasiswa mhs;
+	setNama(mhs, "joko kumat");
+	cout << endl;
+	system("pause");
+	return 0;
+}
